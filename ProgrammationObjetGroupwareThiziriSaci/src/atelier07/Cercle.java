dd1362e934f1bebb341c60ed514d233956c19821
SACI Thizri <SACI Thizri@ThizirisLaptop>
2022-11-24 01:19:17 +0100

    Devoir 4

---
+++ ProgrammationObjetGroupwareThiziriSaci/src/atelier07/Cercle.java
@@ -0,0 +1,52 @@
+package atelier07;
+
+public class Cercle extends FigureGeometrique implements Comparable {
+	private double surface;
+	private double rayonCercle;              //on initialise la variable r du rayon du  cercle
+	//le cercle se définit par sa couleur comme tout autre fugure geométrique et son rayon
+	public Cercle (String co, double rayon) {                         
+		super(co);
+		this.rayonCercle= rayon; 
+	}	
+	@Override
+	public float perimetre() {
+		double perimetre= 2*Math.PI*this.rayonCercle;
+		return  (float) perimetre;
+	}
+	public float getPerimetre() {
+		return this.perimetre();
+	}
+	
+	@Override
+	public float surface() {
+		surface= Math.PI*Math.pow(this.rayonCercle, 2);
+		return (float) surface;
+	}
+	
+	public float getSurface() {
+		return this.surface();
+	}
+	//Implémentation de la méthode compareTo selon le périmètre des cercles
+	@Override
+	public int compareTo(Cercle OtherCercle) {
+		int res;
+		if (this.compareTo(OtherCercle)==0) {
+			res=0;
+			System.out.println("Les deux cercles sont de périmètres égaux");
+		}
+		if (this.compareTo(OtherCercle)>0) {
+			res=1;
+			System.out.println("Le 1e cercle est plus grand que le 2e");
+		}
+		else {
+			res=-1;
+			System.out.println("Le 2e cercle est plus grand que le 1e");
+		}
+		return res;
+		}
+	@Override
+	public int compareTo(Object o) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	}
---
+++ ProgrammationObjetGroupwareThiziriSaci/src/atelier07/testCalendar2.java
@@ -0,0 +1,10 @@
+package atelier07;
+
+public class testCalendar2 {
+	public static void main (String[] args) {
+		Calendar2 testCal= new Calendar2();
+		testCal.creation();
+		testCal.afficher();
+		testCal.duree();
+	}
+}
